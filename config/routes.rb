f3aa4cf61f472620d5c8213cfb45e4f1abc845c7
Mizuking24 <mi_sama.ww1224@icloud.com>
2021-07-13 07:21:05 +0000

    [Fnish] 会員情報編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :customers
   root to: "homes#top"
   get "/about" => "homes#about"
+  get "customers/withdraw" => "customers#withdraw"
+  patch "customers/unsubscribe" => "customers#unsubscribe"
+
+  resources :items, only:[:index, :show]
+  resources :customers, only:[:show, :edit, :update]
+
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_12_042406) do
+ActiveRecord::Schema.define(version: 2021_07_12_061649) do
 
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,11 +25,22 @@ ActiveRecord::Schema.define(version: 2021_07_12_042406) do
     t.string "postal_code"
     t.string "address"
     t.string "telephone_number"
-    t.boolean "is_active", default: false
+    t.boolean "is_active", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.string "image_id"
+    t.text "introduction"
+    t.integer "price"
+    t.boolean "is_active", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
